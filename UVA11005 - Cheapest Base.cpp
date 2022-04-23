55b88748313720b67b448fcc4dc9d9a3de6346db
KoukeFoxes <65907692+KoukeFoxes@users.noreply.github.com>
2022-04-23 18:03:24 +0800

    Update UVA11005 - Cheapest Base.cpp

--- UVA11005 - Cheapest Base.cpp
+++ UVA11005 - Cheapest Base.cpp
@@ -1,39 +1,47 @@
 #include "/Users/doeshing/development/stdc++.h"
 using namespace std;
 
-int main(){
-  int X; //case
+int main()
+{
+  int X; // case
   cin >> X;
-  for(int i = 0 ; i < X ; i++){
+  for (int i = 0; i < X; i++)
+  {
     int price[36];
-    for(int j = 0 ; j < 36 ; j++)
+    for (int j = 0; j < 36; j++)
       cin >> price[j];
     int testcase;
     cin >> testcase;
-    while(testcase--){
+    while (testcase--)
+    {
       int input_number;
       cin >> input_number;
       vector<int> best_base;
       long cheapest = 9999999999;
       int number = input_number;
-      for(int base = 2; base <= 36; base++){
+      for (int base = 2; base <= 36; base++)
+      {
         long sum = 0;
-        while(number > 0){
+        while (number > 0)
+        {
           sum += price[number % base];
           number /= base;
         }
-        if(sum < cheapest) {
+        if (sum < cheapest)
+        {
           best_base.clear();
           best_base.push_back(base);
           cheapest = sum;
-        }else if(sum == cheapest){
+        }
+        else if (sum == cheapest)
+        {
           best_base.push_back(base);
         }
         number = input_number;
       }
       cout << endl;
       cout << "Cheapest base(s) for number " << number << ":";
-      for(int i = 0 ; i < best_base.size() ; i++)
+      for (int i = 0; i < best_base.size(); i++)
         cout << " " << best_base[i];
       cout << endl;
     }
