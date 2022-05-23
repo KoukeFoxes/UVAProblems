ac666cc83ebf69f2606a33a61405d81914449cd7
KoukeFoxes <65907692+KoukeFoxes@users.noreply.github.com>
2022-05-23 19:51:54 +0800

    Update UVA10242 - Fourth Point !!.cpp

--- UVA10242 - Fourth Point !!.cpp
+++ UVA10242 - Fourth Point !!.cpp
@@ -1,60 +1,53 @@
 #include "bits/stdc++.h"
-
 using namespace std;
-bool findTheSamePoint(double pointArr[4][2], double point[2])
-{
-  for (int i = 0; i < 4; i++)
-  {
-    if (pointArr[i][0] == point[0] && pointArr[i][1] == point[1])
-    {
-      return true;
-    }
-  }
-  return false;
+
+double points[4][2];
+double same[2];
+bool foundtheSame;
+int size;
+
+bool isSame(double point[2]){
+	for(int i = 0 ; i < size ; i++){
+		if(point[0] == points[i][0] && point[1] == points[i][1])
+			return true;
+	}
+	return false;
 }
 
-int main()
-{
+int main(){
+	string str;
+	while(getline(cin, str) && str != ""){
+		size = 0;
+		foundtheSame = false;
+		stringstream ss(str);
+		
+		for(size = 0 ; size < 4 ; size++){
+			ss >> points[size][0] >> points[size][1];
+			if(size > 0 && isSame(points[size]) && !foundtheSame){
+				same[0] = points[size][0];
+				same[1] = points[size][1];
+				foundtheSame = true;
+			}
+		}
 
-  while (true)
-  {
-    double same[2];
-    double point[4][2];
-    double temp[2];
-    bool isFindTheSamePoint = false;
+		// for(int i = 0 ; i < 4 ; i++)
+		// 	cout << fixed << setprecision(3) << points[i][0] << " " << points[i][1] << endl;
 
-    // add point to array
-    for (int i = 0; i < 4; i++)
-    {
-      for (int j = 0; j < 2; j++)
-      {
-        cin >> point[i][j];
-      }
-      // check point if same or not
-      if (findTheSamePoint(point, point[i]) && i != 0 && !isFindTheSamePoint) // call by value
-      {
-        same[0] = point[i][0];
-        same[1] = point[i][1];
-        isFindTheSamePoint = true;
-      }
-    }
+		// cout << endl << same[0] << " " << same[1] << endl;
+		
+		double x = 0, y = 0;
+		for(int i = 0 ; i < 4 ; i++ ){
+			x += points[i][0];
+			y += points[i][1];
+		}
 
-    // calculate distance
-    double x = 0, y = 0;
-    for (int i = 0; i < 4; i++)
-    {
-      if (point[i][0] != same[0] || point[i][1] != same[1])
-      {
-        x += point[i][0];
-        y += point[i][1];
-      }
-    }
-    
-    // print same array
-    // cout << same[0] << " " << same[1] << endl;
+		x -= 3* same[0];
+		y -= 3* same[1];
 
-    cout.setf(ios::fixed);
-    cout.precision(3);
-    cout << x - same[0] << " " << y - same[1] << endl;
-  }
-}	
+		cout << fixed << setprecision(3) << x << " " << y << endl;
+		
+	}
+}
+
+
+
